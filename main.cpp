dbd7106e302d2d2da547b80526cc2fab1bb864c3
maintth1807076 <maintth1807076@fpt.edu.vn>
2018-09-25 14:57:25 +0700

    Hoan chinh nhap ho va ten

--- main.cpp
+++ main.cpp
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void noiHoVaTen(char ho[], char ten[]) {
+    printf("Do dai cua ho: %d\n", strlen(ho));
+    int doDaiHo = strlen(ho);
+    printf("Do dai cua ho: ");
+    int doDaiTen = strlen(ten);
+    int tong = doDaiHo + doDaiTen;
+    printf("Do dai ten la: %d\n", tong);
+    tong++;
+    char hovaten[tong];
+    strcpy(hovaten, ho);
+    strcat(hovaten, " ");
+    strcat(hovaten, ten);
+    printf("%s", hovaten);
+}
 
 int main() {
-//    char myName[20];
-    char *pointToChar = (char *) malloc(20 * sizeof(char));
-    *pointToChar = 'x';
-    pointToChar++;
-    *pointToChar = 'u';
-    pointToChar++;
-    *pointToChar = 'a';
-    pointToChar++;
-    *pointToChar = 'n';
-    pointToChar++;
-    *pointToChar = ' ';
-    pointToChar++;
-    *pointToChar = 'h';
-    pointToChar++;
-    *pointToChar = 'u';
-    pointToChar++;
-    *pointToChar = 'n';
-    pointToChar++;
-    *pointToChar = 'g';
-    for (int i = 0; i < 20; ++i) {
-        printf("%c", *pointToChar);
-        pointToChar--;
+    char ho[5], ten[5];
+    printf("Vui long nhap ho cua ban: ");
+    fgets(ho, 5, stdin);
+    puts(ho);
+    if (!strchr(ho, '\n')) {
+        while (fgetc(stdin) != '\n');
     }
+    printf("Vui long nhap ten cua ban: ");
+    fgets(ten, 5, stdin);
+    puts(ten);
+    noiHoVaTen(ho, ten);
     return 0;
 }
