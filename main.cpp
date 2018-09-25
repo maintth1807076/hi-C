101b9e0a1340a4b87906d34a0287f748c8541797
maintth1807076 <maintth1807076@fpt.edu.vn>
2018-09-25 13:45:59 +0700

    Malloc phan 2

--- main.cpp
+++ main.cpp
@@ -2,19 +2,28 @@
 #include <stdlib.h>
 
 int main() {
-    int *pointToArray = (int *) malloc(3 * sizeof(int));
-    if (pointToArray == NULL) {
-        printf("Not enough memory.");
-        return 0;
-    }
-    *pointToArray = 10;
-    pointToArray++;
-    *pointToArray = 20;
-    pointToArray++;
-    *pointToArray = 30;
-    for (int i = 0; i < 3; ++i) {
-        printf("%d\n", *pointToArray);
-        pointToArray--;
+//    char myName[20];
+    char *pointToChar = (char *) malloc(20 * sizeof(char));
+    *pointToChar = 'x';
+    pointToChar++;
+    *pointToChar = 'u';
+    pointToChar++;
+    *pointToChar = 'a';
+    pointToChar++;
+    *pointToChar = 'n';
+    pointToChar++;
+    *pointToChar = ' ';
+    pointToChar++;
+    *pointToChar = 'h';
+    pointToChar++;
+    *pointToChar = 'u';
+    pointToChar++;
+    *pointToChar = 'n';
+    pointToChar++;
+    *pointToChar = 'g';
+    for (int i = 0; i < 20; ++i) {
+        printf("%c", *pointToChar);
+        pointToChar--;
     }
     return 0;
 }
