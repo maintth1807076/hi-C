8a6a0d4076127e929dc347bc6528b3c54625d477
maintth1807076 <maintth1807076@fpt.edu.vn>
2018-09-25 13:30:21 +0700

    Malloc

--- main.cpp
+++ main.cpp
@@ -1,30 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-
-void noiHoVaTen(char ho[], char ten[]) {
-    printf("Do dai cua ho la: %d\n", strlen(ho));
-    printf("Do dai cua ten la: %d\n", strlen(ten));
-    int dodaiHo = strlen(ho);
-    int dodaiTen = strlen(ten);
-    int tong = dodaiHo + dodaiTen;
-    printf("Do dai cua ca ho va ten la %d\n", tong);
-    tong++;
-    char hovaten[tong];
-    strcat(hovaten, ho);
-    strcat(hovaten, " ");
-    strcat(hovaten, ten);
-    printf("%s", hovaten);
-}
+#include <stdlib.h>
 
 int main() {
-    char ho[50], ten[50];
-    printf("Vui long nhap ho cua ban: ");
-    gets(ho);
-    // kiểm tra độ dài họ.
-    printf("Vui long nhap ten cua ban: ");
-    gets(ten);
-    // kiểm tra độ dài tên.
-    printf("Ho va ten cua ban la: ");
-    noiHoVaTen(ho, ten);
+    int *pointToArray = (int *) malloc(3 * sizeof(int));
+    if (pointToArray == NULL) {
+        printf("Not enough memory.");
+        return 0;
+    }
+    *pointToArray = 10;
+    pointToArray++;
+    *pointToArray = 20;
+    pointToArray++;
+    *pointToArray = 30;
+    for (int i = 0; i < 3; ++i) {
+        printf("%d\n", *pointToArray);
+        pointToArray--;
+    }
     return 0;
 }
